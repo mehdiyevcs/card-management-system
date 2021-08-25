fd229c34a1e4279e6506e62b9565a5e4e4fc07dd
Jalal Mehdiyev <calal.mehdiyev@kapitalbank.az>
2021-08-25 20:44:09 +0400

    ms-card microservice finalized

---
+++ ms-card/src/main/java/az/company/card/error/validation/constraints/Pin.java
@@ -0,0 +1,27 @@
+package az.company.card.error.validation.constraints;
+
+import az.company.card.error.validation.ValidationMessage;
+import az.company.card.error.validation.ValidationPattern;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.Pattern;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author MehdiyevCS on 22.08.21
+ */
+@Pattern(regexp = ValidationPattern.PIN, message = ValidationMessage.PIN)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+public @interface Pin {
+    String message() default ValidationMessage.PIN;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
